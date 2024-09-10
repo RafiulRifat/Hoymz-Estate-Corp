a5fd944367306907ada375cd7f4dbecc359c36ee
RafiulRifat <rafiulsiddique301@gmail.com>
2024-09-10 21:17:13 +0600

    Client(new commit)

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import Layout from "./components/Layout/Layout";
+import "./App.css";
+
+import Website from "./pages/website";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+import Properties from "./pages/Properties/Properties";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Website />} />
+
+            <Route path="/properties" element={<Properties />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default App;
