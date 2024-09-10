17d96d76a98521373cdf0878fa5fcf5ad31fde23
RafiulRifat <rafiulsiddique301@gmail.com>
2024-09-11 01:09:42 +0600

    PropertryCard Slider Feature

--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
+
+import "./Properties.css";
+import useProperties from "../../hooks/useProperties";
 
 const Properties = () => {
+  const { data, isError, isLoading } = useProperties();
   return (
     <div className="wrapper">
-      <span>Error while fetching data</span>
+      <div className="flexColCenter paddings innerWidth properties-container">
+        <SearchBar />
+      </div>
     </div>
   );
 };
