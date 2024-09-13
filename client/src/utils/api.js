84175489339a63b2b6c6bd48cb9885ab8b2b7ce9
RafiulRifat <rafiulsiddique301@gmail.com>
2024-09-13 11:16:20 +0600

    Custom hooks

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -123,6 +123,63 @@ export const api = axios.create({
       throw e;
     }
   };
+
+
+
+  export const getAllFav = async (email, token) => {
+    if(!token) return 
+    try{
+  
+      const res = await api.post(
+        `/user/allFav`,
+        {
+          email,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+        
+      return res.data["favResidenciesID"]
+  
+    }catch(e)
+    {
+      toast.error("Something went wrong while fetching favs");
+      throw e
+    }
+  } 
+
+
+
+
+
+  export const getAllBookings = async (email, token) => {
+  
+    if(!token) return 
+    try {
+      const res = await api.post(
+        `/user/allBookings`,
+        {
+          email,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return res.data["bookedVisits"];
+  
+      
+    } catch (error) {
+      toast.error("Something went wrong while fetching bookings");
+      throw error
+    }
+  }
+  
+  
   
   
 
