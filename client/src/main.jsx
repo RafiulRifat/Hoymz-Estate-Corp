c88911b8f3041c60e0351cc280ef9684b3f262ea
RafiulRifat <rafiulsiddique301@gmail.com>
2024-09-20 20:59:15 +0600

    auth0 updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-8iee6ndumnqslye8.us.auth0.com"
       clientId="CQ65ZevqpNNjRt2qsqZYXRKWG0p8EfpE"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173",
+        redirect_uri: "https://hoymz-estate-corp.vercel.app",
       }}
       audience="http://localhost:8000"
       scope="openid profile email"
